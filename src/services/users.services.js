e8c5b1e90fce5e15788cb191613a389cc367dcf8
DarwinQ15 <daquintero91@misena.edu.co>
2022-11-18 21:09:52 -0500

    servs

---
+++ src/services/users.services.js
@@ -0,0 +1,14 @@
+const {Users} = require('../modules');
+
+class userServices {
+    static async create (newUser) {
+        try {
+            const result = await Users.create(newUser)
+            return result
+        } catch (error) {
+            throw error
+        }
+    }
+}
+
+module.exports = {userServices};
